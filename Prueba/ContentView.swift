77b2a2cab77c3236686e87a9a37c068d0e9dfa12
Rafael Magaña <raismaav.2510@gmail.com>
2024-05-01 15:24:20 -0600

    Initial Commit

---
+++ Prueba/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prueba
+//
+//  Created by Rafael Magana  on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Rafael Magana  on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
